6dc8e7f7a1f81eca69989aca10d1b96529edce16
Reeginaa <172979@upf.br>
2024-12-30 09:18:27 -0300

    feat: criação section contato

--- src/pages/Home/section/Contato/Contato.tsx
+++ src/pages/Home/section/Contato/Contato.tsx
@@ -4,7 +4,17 @@ function Contato() {
 
   return (
     <>
-      Section Contato
+      <div className="contato">
+        <div className="container mx-auto">
+          <div className="titulo-contato">Entre em Contato Conosco</div>
+          <div className="descricao-contato">
+            Tem alguma dúvida, precisa de um orçamento ou deseja agendar um serviço? Fique à vontade para falar com a nossa equipe diretamente pelo WhatsApp! Clique no botão abaixo e estamos prontos para atendê-lo com agilidade e qualidade.
+          </div>
+          <a href="#" className="btn-whatsapp">
+            Fale conosco no WhatsApp
+          </a>
+        </div>
+      </div>
     </>
   )
 }
