cfb02ee67c3034a06d116e0d628de16bca44dbbf
Reeginaa <172979@upf.br>
2024-12-29 22:33:38 -0300

    feat: desenvolvimento section Marcas autorizadas

--- src/pages/Home/section/MarcasAutorizadas/MarcasAutorizadas.tsx
+++ src/pages/Home/section/MarcasAutorizadas/MarcasAutorizadas.tsx
@@ -1,10 +1,30 @@
 import "./MarcasAutorizadas.scss"
 
-const MarcasAutorizadas = () => {
+function MarcasAutorizadas() {
 
   return (
     <>
-      Section Marcas Autorizadas
+      <div className="marcas-autorizadas">
+        <div className="header-marcas-autorizadas">
+          <span className="linha"></span>
+          <div className="titulo-marcas-autorizadas">Marcas Autorizadas</div>
+          <div className="subtitulo-marcas-autorizadas">Conheça as marcas autorizadas com que trabalhamos</div>
+        </div>
+        <div className="carrossel-marcas">
+          <div className="marca">
+            <img src="src/assets/images/marcas-autorizadas/marca.png" alt="Marca 1" />
+            <div className="nome-marca">Marca 1</div>
+          </div>
+          <div className="marca">
+            <img src="src/assets/images/marcas-autorizadas/marca.png" alt="Marca 1" />
+            <div className="nome-marca">Marca 2</div>
+          </div>
+          <div className="marca">
+            <img src="src/assets/images/marcas-autorizadas/marca.png" alt="Marca 1" />
+            <div className="nome-marca">Marca 3</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
