a6a5195987f696adf058f52ea6189323696de76f
Reeginaa <172979@upf.br>
2024-12-29 22:33:55 -0300

    feat: desenvolvimento section depoimentos de clientes

--- src/pages/Home/section/Clientes/Clientes.tsx
+++ src/pages/Home/section/Clientes/Clientes.tsx
@@ -1,10 +1,56 @@
 import "./Clientes.scss"
 
-const Clientes = () => {
+function Clientes() {
 
   return (
     <>
-      Section Clientes
+      <div className="clientes">
+        <div className="carrossel-feedbacks">
+          <div className="feedback">
+            <div className="cabecalho-feedback">
+              <img src="src/assets/images/clientes/cliente.png" alt="Foto cliente" />
+              <div className="nome-avaliacao">
+                <div className="nome">Nome do Cliente</div>
+                <div className="avaliacao">
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash-outline.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash-outline.svg" alt="Flash" />
+                </div>
+              </div>  
+            </div>
+            <div className="depoimento-cliente">
+            Lorem ipsum dolor sit amet consectetur. Tortor tortor arcu porttitor tempus in. In quam orci in tortor dictum blandit. Feugiat amet sed malesuada pulvinar scelerisque metus sit risus. Integer ligula eget pulvinar diam viverra commodo commodo magna mauris.Lorem ipsum dolor sit amet consectetur. Tortor tortor arcu porttitor tempus in. In quam orci in tortor dictum blandit. Feugiat amet sed malesuada pulvinar scelerisque metus sit risus. Integer ligula eget pulvinar diam viverra commodo commodo magna mauris.
+            </div>
+          </div>
+
+          <div className="feedback">
+            <div className="cabecalho-feedback">
+              <img src="src/assets/images/clientes/cliente.png" alt="Foto cliente" />
+              <div className="nome-avaliacao">
+                <div className="nome">Nome do Cliente</div>
+                <div className="avaliacao">
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash-outline.svg" alt="Flash" />
+                  <img src="src/assets/images/clientes/flash-outline.svg" alt="Flash" />
+                </div>
+              </div>  
+            </div>
+            <div className="depoimento-cliente">
+            Lorem ipsum dolor sit amet consectetur. Tortor tortor arcu porttitor tempus in. In quam orci in tortor dictum blandit. Feugiat amet sed malesuada pulvinar scelerisque metus sit risus. Integer ligula eget pulvinar diam viverra commodo commodo magna mauris.Lorem ipsum dolor sit amet consectetur. Tortor tortor arcu porttitor tempus in. In quam orci in tortor dictum blandit. Feugiat amet sed malesuada pulvinar scelerisque metus sit risus. Integer ligula eget pulvinar diam viverra commodo commodo magna mauris.
+            </div>
+          </div>
+        </div>
+
+        <div className="header-clientes">
+          <span className="linha"></span>
+          <div className="titulo-clientes">Feedbacks</div>
+          <div className="subtitulo-clientes">O que nossos clientes dizem...</div>
+        </div>
+      </div>
     </>
   )
 }
