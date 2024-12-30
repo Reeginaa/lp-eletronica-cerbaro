5171fb0aa7eeab6f650ab90f2283e172fa9fe22e
Reeginaa <172979@upf.br>
2024-12-30 09:01:08 -0300

    feat: inicio section quem somos, sem efeitos ainda

--- src/pages/Home/section/QuemSomos/QuemSomos.tsx
+++ src/pages/Home/section/QuemSomos/QuemSomos.tsx
@@ -4,7 +4,29 @@ function QuemSomos() {
 
   return (
     <>
-      Section Quem Somos
+      <div className="quem-somos">
+        <div className="header-quem-somos">
+          <span className="linha"></span>
+          <div className="texto-quem-somos">
+            <div className="titulo-quem-somos">Quem somos</div>
+            <div className="subtitulo-quem-somos">Conheça mais da história da Eletrônica Cerbaro e sua equipe!</div>
+          </div>
+          <div></div>
+        </div>
+        <div className="container mx-auto">
+          <div className="imagens">
+            <img src="src/assets/images/quem-somos/quem-somos.png" alt="Quem Somos" />
+            <img src="src/assets/images/quem-somos/quem-somos.png" alt="Quem Somos" />
+            <img src="src/assets/images/quem-somos/quem-somos.png" alt="Quem Somos" />
+          </div>
+          <div className="texto">
+            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
+            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
+            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
+            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque 
+          </div>
+        </div>
+      </div>
     </>
   )
 }
