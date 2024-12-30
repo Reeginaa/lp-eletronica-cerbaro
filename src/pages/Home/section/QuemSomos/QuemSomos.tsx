c9e9869a59f2914d1456ccd7aabf38e5f3ffdccc
Reeginaa <172979@upf.br>
2024-12-30 15:36:03 -0300

    style: ponto animado na section quem somos

--- src/pages/Home/section/QuemSomos/QuemSomos.tsx
+++ src/pages/Home/section/QuemSomos/QuemSomos.tsx
@@ -6,7 +6,10 @@ function QuemSomos() {
     <>
       <div id="quem-somos" className="quem-somos">
         <div className="header-quem-somos">
-          <span className="linha"></span>
+          <div className="ponto-animado">
+            <div className="ponto"></div>
+            <span className="linha"></span>
+          </div>
           <div className="texto-quem-somos">
             <div className="titulo-quem-somos">Quem somos</div>
             <div className="subtitulo-quem-somos">Conheça mais da história da Eletrônica Cerbaro e sua equipe!</div>
@@ -23,7 +26,7 @@ function QuemSomos() {
             Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
             Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
             Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque at etiam eget tristique enim orci. Amet neque pretium gravida interdum sem blandit amet gravida sit. Vitae libero eu malesuada sit volutpat massa cras. Commodo dui risus habitant nunc lacus.
-            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque 
+            Lorem ipsum dolor sit amet consectetur. Sit sed aliquam eget proin sem diam. Quam sed sed pulvinar at pellentesque. Nulla vitae sit tellus semper egestas pellentesque pulvinar tellus. Rutrum pretium pellentesque
           </div>
         </div>
       </div>
